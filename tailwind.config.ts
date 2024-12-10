04e01bb8f6109d9b35dd7e5bd5cac79e864da0b6
Igor Casconi <igor492@gmail.com>
2024-12-10 19:26:18 -0300

    feat(middleware): config routes and important files added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,30 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: "#ffffff",
+        black: "#000000",
+        gray: {
+          100: "#E0E0E0",
+          200: "#D0D0D0",
+          300: "#C8C8C8",
+          400: "#B8B8B8",
+          500: "#A0A0A0",
+          600: "#888888",
+          700: "#686868",
+        },
+        primary: "#1D1D1D",
+        secondary: "#CC6726",
+        negative: {
+          100: "#c25848",
+          200: "#bf3e2a",
+          300: "#ba230b",
+        },
       },
     },
   },
