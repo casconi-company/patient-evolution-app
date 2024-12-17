2bd3c2344740753123e2713d87c26b6fe99066b9
Igor Casconi <igor492@gmail.com>
2024-12-16 21:36:16 -0300

    feat(menu): component menu added

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button, Header, Input, Menu } from "@/components";
+import Image from "next/image";
+
+export default function Login() {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <Header />
+    </div>
+  );
+}
