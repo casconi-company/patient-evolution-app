1609bc780807e05be9709726fc6c6f4679f80ccb
AllanZpt <allanzapaterra@hotmail.com>
2024-12-30 20:54:16 -0300

    Add get by ID

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,28 @@
+import { getUserData } from "@/firebase/database/users";
+
+export async function GET(req: Request, route: { params: { id: string } }) {
+  const { id } = route.params;
+  console.log(id);
+  if (typeof id !== "string") {
+    return new Response(JSON.stringify({ message: "Invalid User ID" }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const userData = await getUserData(id);
+    console.log(userData);
+    if (!userData) {
+      return new Response(JSON.stringify({ message: "User not found" }), {
+        status: 400,
+      });
+    }
+
+    return new Response(JSON.stringify({ data: userData }), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    return new Response(JSON.stringify({ message: "Internal server error" }), {
+      status: 400,
+    });
+  }
+}
