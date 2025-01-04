b85bd94840755516c7f54773157f3d793700cb29
Igor Casconi <igor.oliveira@nav9.tech>
2025-01-04 16:10:11 -0300

    feat(users): users screen added

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,22 +1,26 @@
 "use client";
 
-import { Button } from "@/components";
+import { Button, Input } from "@/components";
 import { useRouter } from "next/navigation";
 
 export default function Users() {
   const router = useRouter();
 
   return (
-    <div className="w-full px-4 pt-4 flex justify-center items-center h-full max-sm:pb-20 max-sm:pt-4 max-sm:overflow-auto lg:px-8 min-md:w-[1000px]">
-      <div className="w-full flex flex-row justify-end">
+    <div className="w-full px-4 pt-6 flex flex-col items-center h-full max-sm:pb-20 max-sm:pt-4 max-sm:overflow-auto lg:px-10 min-md:w-[1000px]">
+      <div className="w-full flex flex-row justify-between">
+        <h2 className="text-white text-2xl">Usuários</h2>
         <Button
           className="!w-[250px] max-md:!w-full"
           text="Cadastrar novo usuário"
           onClick={() => router.push("/admin/register")}
         />
       </div>
+      <div className="mt-4 w-full">
+        <Input placeholder="pesquisar por email do usuário" />
+      </div>
 
-      <div className=""></div>
+      <div className="mt-6"></div>
     </div>
   );
 }
